fccffda1f640d2e9e1a98e2dc462992f2bb73d60
kumar-krishna1820 <119351236+kumar-krishna1820@users.noreply.github.com>
2023-03-25 12:40:45 +0530

    Add files via upload
    Python Socket programming file

---
+++ python_socket.py
@@ -0,0 +1,10 @@
+import socket
+serverIP="172.17.29.11"
+serverPORT=6000
+serveraddress = (serverIP,serverPORT)
+bufferSize=1024
+UDPClientSocket=socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
+message="Hi my name is Kumar Krishna"
+bytestosend=str.encode(message)
+UDPClientSocket.sendto(bytestosend,serveraddress)
+
